b6571e1dc58260cc482eee1d0a0c407de351b97b
Nivesh Shukla <52473549+NiviShukla@users.noreply.github.com>
2023-07-06 14:15:04 +0530

    Initial commit

---
+++ src/main/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from .models import Listing
+
+
+class ListingForm(forms.ModelForm):
+    image = forms.ImageField()
+
+    class Meta:
+        model = Listing
+        fields = {'brand', 'model', 'vin', 'mileage',
+                  'color', 'description', 'engine', 'transmisson', 'image'}
